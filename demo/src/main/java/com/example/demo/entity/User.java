02912dc95e30d0afb3b31073cf4c04b24b906c14
sinueh-andreata <sinuehnoan@gmail.com>
2025-12-04 14:30:12 -0300

    feat: user Entity

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.demo.entity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+    
+    @CreationTimestamp
+    private Instant creationTimestamp;
+    
+    @UpdateTimestamp
+    private Instant updatedTimestamp;
+
+    public User() {
+    }
+
+    public User(UUID userId, String username, String email, String password, Instant creationTimestamp, Instant updatedTimestamp) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.creationTimestamp = creationTimestamp;
+        this.updatedTimestamp = updatedTimestamp;
+    }
+}
