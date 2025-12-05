ee038ce1972e7db6736eb2786dd42cda68527449
sinuehpassa <sinuehnoan@gmail.com>
2025-12-04 23:28:27 -0300

    feat: controller and service to create user

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.User;
+import com.example.demo.services.UserService;
+import com.example.demo.dto.CreateUsersDto;
+import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import java.net.URI;
+
+
+// apí controller
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    // injetando o servico de usuario
+    private UserService userService;
+    
+    // construtor para injetar o servico de usuario
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // endpoint para criar um novo usuario
+    @PostMapping // metodo post
+    public ResponseEntity<User> createUser(@RequestBody CreateUsersDto createUsersDto) { // aqui eu recebo o dto como corpo da requisicao
+        var userId = userService.createUser(createUsersDto); // chamando o servico para criar o usuario
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build(); 
+    }
+
+    // @GetMapping("/{userId}")
+    // public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
+    //     // Logic to get a user by ID
+    // }
+
+    // @PutMapping
+    // public ResponseEntity<User> createUser(@RequestBody User user) {
+    //     // Logic to create a new user
+    // }
+
+    // @DeleteMapping
+    // public ResponseEntity<User> createUser(@RequestBody User user) {
+    //     // Logic to create a new user
+    // }
+}
---
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,36 @@
+package com.example.demo.services;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.User;
+import java.time.Instant;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import com.example.demo.dto.CreateUsersDto;
+
+
+@Service
+public class UserService {
+
+    // injetando o repositorio de usuario
+    private UserRepository userRepository;
+
+    // construtor para injetar o repositorio de usuario
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // metodo para criar um novo usuario
+    public UUID createUser(CreateUsersDto createUsersDto) {
+
+        var entity = new User(
+                UUID.randomUUID(),
+                createUsersDto.username(),
+                createUsersDto.email(),
+                createUsersDto.password(),
+                Instant.now(),
+                null);
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
